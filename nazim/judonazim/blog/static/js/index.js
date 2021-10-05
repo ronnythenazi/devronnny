7b7c5a3fe8a14c1aaba86745c9fddf75c717ca7a
רונן סבו <ronnythenazi@gmail.com>
2021-10-06 02:20:39 +0300

    improved vertical navbar, and also implment grid layout to create nice subtitle with 2 logos on its side rotating(i use jqeury to rotate them

--- nazim/judonazim/blog/static/js/index.js
+++ nazim/judonazim/blog/static/js/index.js
@@ -3,7 +3,54 @@
 //aimed to open hidden box when item has been clickedin nav bar
 //nextall select the first matched element after the ellment
 //in this case, after the clicked element
+$(document).ready(fload);
 
+function fload()
+{
+
+
+setInterval(function() {
+//console.log("timer!");
+rotate($('.logoright'));
+},100);
+
+setInterval(function() {
+//console.log("timer!");
+rotate($('.logoleft'),-1);
+},100);
+
+}
+
+
+function rotate(elem, dir=1)
+{
+
+    var angel = getRotationDegrees(elem);
+    elem.animate(
+    { deg:(angel + (100*dir))},
+    {
+      duration: 1,
+      step: function(now) {
+        $(this).css({ transform: 'rotate(' + now + 'deg)' });
+      }
+    }
+  );
+}
+
+function getRotationDegrees(obj) {
+    var matrix = obj.css("-webkit-transform") ||
+    obj.css("-moz-transform")    ||
+    obj.css("-ms-transform")     ||
+    obj.css("-o-transform")      ||
+    obj.css("transform");
+    if(matrix !== 'none') {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    } else { var angle = 0; }
+    return (angle < 0) ? angle + 360 : angle;
+}
 
 $(".sub_subject_box").click(
   function ()
