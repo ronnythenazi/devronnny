4b0d8f232f03493508400c08179b9390d738e101
unknown <ronnythenazi@gmail.com>
2021-11-28 03:45:17 +0200

    back to previous setting

--- nazim/judonazim/judonazim/cdn/conf.py
+++ nazim/judonazim/judonazim/cdn/conf.py
@@ -6,8 +6,7 @@ AWS_S3_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {
   "CacheControl": "max-age=86400",
 }
-#AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.fra1.digitaloceanspaces.com"
-AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.fra1.cdn.digitaloceanspaces.com"
+AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.fra1.digitaloceanspaces.com"
 
 
 DEFAULT_FILE_STORAGE = "judonazim.cdn.backends.MediaRootS3Boto3Storage"
