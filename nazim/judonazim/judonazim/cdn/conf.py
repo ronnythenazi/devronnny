c5d499ecfdcde03b61d7d7f5f0d0007c79537c72
unknown <ronnythenazi@gmail.com>
2021-11-28 03:32:50 +0200

    change mispelling error

--- nazim/judonazim/judonazim/cdn/conf.py
+++ nazim/judonazim/judonazim/cdn/conf.py
@@ -3,7 +3,7 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 #AWS_S3_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
-AWS_S3_ENDPOINT_UR   = "https://ronnythenazi.fra1.cdn.digitaloceanspaces.com"
+AWS_S3_ENDPOINT_URL   = "https://ronnythenazi.fra1.cdn.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {
   "CacheControl": "max-age=86400",
 }
