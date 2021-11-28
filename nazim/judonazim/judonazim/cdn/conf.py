db1195c97afbab6e902673dd056f9132bf236def
unknown <ronnythenazi@gmail.com>
2021-11-28 03:43:15 +0200

    .cdn to aws location

--- nazim/judonazim/judonazim/cdn/conf.py
+++ nazim/judonazim/judonazim/cdn/conf.py
@@ -2,12 +2,12 @@ import os
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-#AWS_S3_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
-AWS_S3_ENDPOINT_URL   = "https://ronnythenazi.fra1.cdn.digitaloceanspaces.com"
+AWS_S3_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {
   "CacheControl": "max-age=86400",
 }
-AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.fra1.digitaloceanspaces.com"
+#AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.fra1.digitaloceanspaces.com"
+AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.fra1.cdn.digitaloceanspaces.com"
 
 
 DEFAULT_FILE_STORAGE = "judonazim.cdn.backends.MediaRootS3Boto3Storage"
