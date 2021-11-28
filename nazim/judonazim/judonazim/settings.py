8ca85c7b982199fa9ddedd33111bf7e0d12f15e7
unknown <ronnythenazi@gmail.com>
2021-11-28 10:28:18 +0200

    /media-cdn

--- nazim/judonazim/judonazim/settings.py
+++ nazim/judonazim/judonazim/settings.py
@@ -165,7 +165,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/mediafiles-cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media-cdn')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
