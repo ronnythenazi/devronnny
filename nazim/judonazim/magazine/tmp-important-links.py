9d8bd6a390b2baac3b454bb97ac4fb073b05e412
ronnythenazi <ronnythenazi@gmail.com>
2021-12-22 19:31:53 +0200

    change debog=0

--- nazim/judonazim/magazine/tmp-important-links.py
+++ nazim/judonazim/magazine/tmp-important-links.py
@@ -4,3 +4,4 @@
 #https://www.youtube.com/playlist?list=PLCC34OHNcOtr025c1kHSPrnP18YPB-NFi #create a simple blog, full tutorial, including aythonitcation, and
 #mulitply ckeditors with different height property https://stackoverflow.com/questions/11124182/how-to-set-up-ckeditor-for-multiple-instances-with-different-heights
 #judonazim-z6fn3.ondigitalocean.app
+# generate random password django python -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
