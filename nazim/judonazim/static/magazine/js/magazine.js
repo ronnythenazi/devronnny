371feceffa1d7d14bbed49ca2cf6fe111f74659c
ronnythenazi <ronnythenazi@gmail.com>
2022-01-03 23:02:29 +0200

    fixing bug at home page

--- nazim/judonazim/static/magazine/js/magazine.js
+++ nazim/judonazim/static/magazine/js/magazine.js
@@ -1,2 +1,8 @@
 $('.menuItem').click(function(){$(this).children('div').css('visibility', 'visible');});
 $('.menuItem>div').mouseleave(function(){$(this).css('visibility', 'hidden');});
+$('.more-posts-link').mouseover(function(){
+  $(this).find('.bi-chevron-double-left').css('fill', 'yellow');
+});
+$('.more-posts-link').mouseleave(function(){
+  $(this).find('.bi-chevron-double-left').css('fill', 'orange');
+});
