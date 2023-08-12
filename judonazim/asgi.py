14d76c39306b1a5eaddd5486af881ac1d9a203d9
ronnythenazi <ronnythenazi@gmail.com>
2023-08-13 02:09:08 +0300

    modified asgi because there is issue with os.environ.setdefault(DJANGO_SETTINGS_MODULE, judonazim.settings) apperantely the run time error log in production was ImproperlyConfigured: You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() before accessing settings

--- judonazim/asgi.py
+++ judonazim/asgi.py
@@ -1,13 +1,19 @@
 import os
 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "judonazim.settings")
+import django
+django.setup()
+
+
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "judonazim.settings")
+
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
